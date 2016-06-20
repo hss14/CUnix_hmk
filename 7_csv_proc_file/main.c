51600fe08a2ee136724fd9b70e93c888015236b8
hss14 <aqyz0713hss@163.com>
2016-06-20 16:02:13 +0800

    some typo corrected

--- 7_csv_proc_file/main.c
+++ 7_csv_proc_file/main.c
@@ -7,8 +7,8 @@ int main( int argc, char *argv[] )
 	if( argc < 3 )
 	{
 		printf("illegal argument!\n");
-		printf("for transform csv file to bin file, use command:  \"./csv6 t name_of_csv_file name_of_bin_file\"\n");
-		printf("for print out contents of bin flie, use command:  \"./csv6 p name_of_bin_file(s)\"\n");
+		printf("for transform csv file to bin file, use command:  \"./csv7 t name_of_csv_file name_of_bin_file\"\n");
+		printf("for print out contents of bin flie, use command:  \"./csv7 p name_of_bin_file(s)\"\n");
 		exit(0);
 	}
 	
@@ -18,7 +18,7 @@ int main( int argc, char *argv[] )
 		if( argc < 4 )
 		{
 			printf("argument filename_bin missing!\nlegal example for transform:\n");
-			printf("./csv6 t filename_csv filename_bin\n");
+			printf("./csv7 t filename_csv filename_bin\n");
 			exit(0);
 		}
 		if( (vreturn = csv2bin( argv[2], argv[3] ) ) != 0 )
